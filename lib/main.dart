4a6e3633b9f5794b7fe0c9ccb23da4650f27a94d
sarvesh4396 <heysarvesh@pm.me>
2022-04-03 23:58:49 +0530

    completed app

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,32 @@
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:agorasampleapp/settings.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  AgoraClient client = AgoraClient(
+      agoraConnectionData: AgoraConnectionData(appId: appId, channelName: "test"),
+      enabledPermission: [Permission.camera, Permission.microphone]);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sample Agora App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Agora Sample App")),
+        body: Stack(
+          children: [
+            AgoraVideoViewer(client: client),
+            AgoraVideoButtons(client: client)
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
